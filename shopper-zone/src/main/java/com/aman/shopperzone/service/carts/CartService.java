8f0e26d3882b46ef77113b64f1199a46fd74ae9c
Aman Maurya <amanmaurya.2709@gmail.com>
2025-11-09 12:37:02 +0530

    Fixed issue for Recursive JSON Response and for clear-cart endpoint to save data using transaction manager

--- shopper-zone/src/main/java/com/aman/shopperzone/service/carts/CartService.java
+++ shopper-zone/src/main/java/com/aman/shopperzone/service/carts/CartService.java
@@ -4,6 +4,7 @@ import com.aman.shopperzone.exceptions.ResourceNotFoundException;
 import com.aman.shopperzone.model.Cart;
 import com.aman.shopperzone.repository.CartItemRepository;
 import com.aman.shopperzone.repository.CartRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,7 @@ public class CartService implements ICartService{
         return cartRepository.save(cart);
     }
 
+    @Transactional
     @Override
     public void clearCart(Long id) {
         Cart cart = getCart(id);
