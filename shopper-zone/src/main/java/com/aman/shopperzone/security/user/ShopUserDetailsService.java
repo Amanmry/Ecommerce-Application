43e7f036e71a2305775d4615532a5af48c38e79e
Aman Maurya <amanmaurya.2709@gmail.com>
2025-11-11 08:17:00 +0530

    Added Roles and User Relationship, UserDetails, UserDetailsService

---
+++ shopper-zone/src/main/java/com/aman/shopperzone/security/user/ShopUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.aman.shopperzone.security.user;
+
+import com.aman.shopperzone.exceptions.ResourceNotFoundException;
+import com.aman.shopperzone.model.User;
+import com.aman.shopperzone.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
+}
